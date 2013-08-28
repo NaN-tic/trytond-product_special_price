9c23087ce9c2016573763c2f734a7b2bd804dc18
resteve <resteve@zikzakmedia.com>
2013-08-28 17:37:33 +0200

    FIX compute price from price list

--- product.py
+++ product.py
@@ -26,28 +26,28 @@ class Product:
 
     @classmethod
     def get_sale_price(cls, products, quantity=0):
-        User = Pool().get('res.user')
-        PriceList = Pool().get('product.price_list')
         prices = super(Product, cls).get_sale_price(products, quantity)
+
         if (Transaction().context.get('customer')):
+            User = Pool().get('res.user')
+            PriceList = Pool().get('product.price_list')
+
             user = User(Transaction().user)
             if user.shop and user.shop.special_price:
                 for product in products:
                     special_price = 0.0
                     if user.shop.type_special_price == 'pricelist':
-                        print "dins2"
-                        special_price = PriceList.compute(
-#                                Transaction().context['price_list'],
-                                user.shop.special_pricelist,
-                                Transaction().context['customer'],
-                                product,
-                                product.list_price,
-                                quantity,
-                                Transaction().context.get('uom',
-                                        product.default_uom))
+                        price_list = PriceList(user.shop.special_pricelist)
+                        customer = Transaction().context['customer']
+                        uom = Transaction().context.get('uom', product.default_uom)
+
+                        special_price = price_list.compute(customer, product,
+                            prices[product.id], quantity, uom)
                     else:
                         special_price = product.special_price
+
                     if special_price != 0.0 and special_price != None and \
                             special_price < prices[product.id]:
                         prices[product.id] = special_price
+
         return prices
