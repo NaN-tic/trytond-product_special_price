efb4f7c83d884a7b342bcf5299c81a5759ac521a
resteve <resteve@zikzakmedia.com>
2014-05-21 09:59:53 +0200

    Special price list required when type is price list

--- shop.py
+++ shop.py
@@ -21,8 +21,8 @@ class SaleShop:
             }, depends=['special_price'])
     special_pricelist = fields.Many2One('product.price_list', 
             'Special Pricelist', states={
-                'required': Eval('special_price', True),
-            }, depends=['special_price'])
+                'required': Eval('type_special_price') == 'pricelist',
+            }, depends=['type_special_price'])
 
     @staticmethod
     def default_type_special_price():
