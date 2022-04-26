7007cd7ad4df27757a52186968ac7f574e4de8c7
Jared Esparza <jared.esparza@nan-tic.com>
2022-04-26 13:26:06 +0200

    Replace test setuptools command by unittest discover

---
+++ tests/test_module.py
@@ -0,0 +1,14 @@
+
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+
+from trytond.modules.company.tests import CompanyTestMixin
+from trytond.tests.test_tryton import ModuleTestCase
+
+
+class ProductSpecialPriceTestCase(CompanyTestMixin, ModuleTestCase):
+    'Test ProductSpecialPrice module'
+    module = 'product_special_price'
+
+
+del ModuleTestCase
