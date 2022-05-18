24f6d77d9fc07ca2349b6c882f6fe14db27a708f
Jared Esparza <jared.esparza@nan-tic.com>
2022-05-18 14:07:17 +0200

    Use SaleShopCompanyTestMixin

--- tests/test_module.py
+++ tests/test_module.py
@@ -2,11 +2,11 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 
-from trytond.modules.company.tests import CompanyTestMixin
+from trytond.modules.sale_shop.tests import SaleShopCompanyTestMixin
 from trytond.tests.test_tryton import ModuleTestCase
 
 
-class ProductSpecialPriceTestCase(CompanyTestMixin, ModuleTestCase):
+class ProductSpecialPriceTestCase(SaleShopCompanyTestMixin, ModuleTestCase):
     'Test ProductSpecialPrice module'
     module = 'product_special_price'
     extras = ['product_esale']
