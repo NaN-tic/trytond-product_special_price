a62cdbe624a30943d7f6d250f72a14a0310687d2
Albert Cervera i Areny <albert@nan-tic.com>
2023-10-11 12:12:27 +0200

    Pyflakes fixes

--- product.py
+++ product.py
@@ -1,7 +1,6 @@
 # This file is part of product_special_price module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond import backend
 from trytond.pool import Pool, PoolMeta
 from trytond.model import ModelSQL, fields
 from trytond.pyson import Eval
